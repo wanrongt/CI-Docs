ec8f39e736931da4ec419da3999e2e067c488582
Wanrong <wanrong.tan@intel.com>
2021-07-29 16:54:46 +0800

    update

--- BMC CI Docs/doc/source/conf.py
+++ BMC CI Docs/doc/source/conf.py
@@ -39,7 +39,7 @@ release = '1.0'
 # extensions = [
 # ]
 
-extensions = ['recommonmark','sphinx_markdown_tables']
+extensions = ['recommonmark']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
